18d879c61459b40f377c6f95884b388a9f6dbc09
Max Castaneda <maxcastanedaiv@gmail.com>
2018-11-08 18:52:56 -0500

    Hello Message

--- app/src/main/java/edu/fsu/cs/mobile/hw5/groupone/MainActivity.java
+++ app/src/main/java/edu/fsu/cs/mobile/hw5/groupone/MainActivity.java
@@ -8,6 +8,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
+
+        //Hello
         setContentView(R.layout.activity_main);
     }
 }
