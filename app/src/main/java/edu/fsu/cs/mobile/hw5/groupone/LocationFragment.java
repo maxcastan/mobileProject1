8432963b01cdffe0e1d6f48dc7295deadbfd4787
camirios <cr15h@my.fsu.edu>
2018-11-17 16:17:14 -0500

    delete old map
    must delete, errors

--- app/src/main/java/edu/fsu/cs/mobile/hw5/groupone/LocationFragment.java
+++ app/src/main/java/edu/fsu/cs/mobile/hw5/groupone/LocationFragment.java
@@ -22,18 +22,6 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdate;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
-
 
 public class LocationFragment extends Fragment implements ActivityCompat.OnRequestPermissionsResultCallback{
 
